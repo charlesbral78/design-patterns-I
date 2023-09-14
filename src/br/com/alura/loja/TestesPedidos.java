7808a0221451a737676a7eaeca4e2e6ee8d4842f
â[fcharles <charlesbral78@hotmail.com>
2023-09-14 12:52:58 -0300

    Aplicando o Patterns Observer

--- src/br/com/alura/loja/TestesPedidos.java
+++ src/br/com/alura/loja/TestesPedidos.java
@@ -4,20 +4,28 @@ import br.com.alura.loja.orcamento.Orcamento;
 import br.com.alura.loja.pedido.GeraPedido;
 import br.com.alura.loja.pedido.GeraPedidoHandler;
 import br.com.alura.loja.pedido.Pedido;
+import br.com.alura.loja.pedido.acao.EnviarEmailPedido;
+import br.com.alura.loja.pedido.acao.SalvarPedidoBancoDados;
 
+import java.lang.reflect.Array;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.Arrays;
 
 
 public class TestesPedidos {
 
     public static void main(String[] args) {
-        String cliente = args[0];
-        BigDecimal valorOrcamento = new BigDecimal(args[1]);
-        int quantidadeItens = Integer.parseInt(args[2]);
+        String cliente = "Charles Cabral";
+        BigDecimal valorOrcamento = new BigDecimal("500");
+        int quantidadeItens = Integer.parseInt("4");
 
         GeraPedido gerador = new GeraPedido(cliente, valorOrcamento, quantidadeItens);
-        GeraPedidoHandler handler = new GeraPedidoHandler(/*dependencias*/);
+        GeraPedidoHandler handler = new GeraPedidoHandler(
+                Arrays.asList(new SalvarPedidoBancoDados(),
+                              new EnviarEmailPedido()
+                ));
+
         handler.execute(gerador);
 
     }
--- src/br/com/alura/loja/pedido/GeraPedidoHandler.java
+++ src/br/com/alura/loja/pedido/GeraPedidoHandler.java
@@ -1,10 +1,17 @@
 package br.com.alura.loja.pedido;
 
 import br.com.alura.loja.orcamento.Orcamento;
-
+import br.com.alura.loja.pedido.acao.AcaoAposGerarPedido;
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class GeraPedidoHandler {
+
+    private List<AcaoAposGerarPedido> acoes;
+
+    public GeraPedidoHandler(List<AcaoAposGerarPedido> acoes) {
+        this.acoes = acoes;
+    }
 
     //contrutor com injecao de dependencias: repository, service, etc...
 
@@ -13,8 +20,8 @@ public class GeraPedidoHandler {
 
         Pedido pedido = new Pedido(dados.getCliente(), LocalDateTime.now(), orcamento);
 
-        System.out.println("Salvar pedido no banco de dados");
-        System.out.println("Enviar email com dados do novo pedido");
+        acoes.forEach(a -> a.executarAcao(pedido));
+
     }
     }
 
